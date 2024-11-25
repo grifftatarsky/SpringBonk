f839613500f7932a918cadcb1f05b94a8759a746
gtatarsky-IB <griffith.tatarsky@intellibridge.us>
2024-11-25 01:26:49 -0500

    UGH

---
+++ spring-bff/src/test/java/com/gpt/bonk_bff/BffApplicationIntegrationTest.java
@@ -0,0 +1,39 @@
+package com.gpt.bonk_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithAnonymousUser;
+
+import com.c4_soft.springaddons.security.oauth2.test.webflux.AddonsWebfluxTestConf;
+import com.c4_soft.springaddons.security.oauth2.test.webflux.WebTestClientSupport;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+@ImportAutoConfiguration(AddonsWebfluxTestConf.class)
+class BffApplicationIntegrationTest {
+
+    // IDE Warning is irrelevant; bean is present, and functional.
+    @Autowired
+    WebTestClientSupport api;
+
+    @Test
+    @WithAnonymousUser
+    void givenRequestIsAnonymous_whenGetLoginOptions_thenOk()
+    {
+        api.get("/login-options")
+            .expectStatus()
+            .isOk()
+            .expectBody()
+            .jsonPath("$")
+            .isArray()
+            .jsonPath("$[0].label")
+            .isEqualTo("gpt-keycloak")
+            .jsonPath("$[0].loginUri")
+            .isEqualTo("https://localhost:7080/bff/oauth2/authorization/gpt-keycloak")
+            .jsonPath("$[0].isSameAuthority")
+            .isEqualTo(true);
+    }
+}
---
+++ spring-resource/src/test/java/com/gpt/springbonk/SpringBonkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gpt.springbonk;
+
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class SpringBonkApplicationTests
+{
+  // Uh. No tests?
+}
