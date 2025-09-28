cd02e47ae7f90291dfb7cd2e1c12e49cce2b27d9
gtatarsky-IB <griff.tatarsky@noaa.gov>
2025-09-27 22:51:49 -0400

    fuck it. clean slate

--- angular-client/ng-bonk/src/app/model/response/election-paged-response.model.ts
+++
@@ -1,5 +0,0 @@
-import { PagedResponse } from './paged-response.model';
-import { ElectionResponse } from './election-response.model';
-
-export interface ElectionPagedResponse
-  extends PagedResponse<ElectionResponse> {}
