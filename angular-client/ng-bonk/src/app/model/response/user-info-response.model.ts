038be2747652244c7f754ec3d8da13fb63d155db
gtatarsky-IB <griff.tatarsky@noaa.gov>
2025-09-28 01:13:09 -0400

    cleanslate

--- angular-client/ng-bonk/src/app/model/response/user-info-response.model.ts
+++
@@ -1,7 +0,0 @@
-export interface UserInfoResponse {
-  id: string; // UUID string (token subject)
-  username: string; // human-friendly username
-  email: string;
-  roles: string[];
-  exp: number;
-}
